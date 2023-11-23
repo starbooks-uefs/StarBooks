308c4d042324523475fd2266d0976cc8dc4d2d13
Ricardo Campos <rickoliver001@hotmail.com>
2023-11-23 20:27:44 -0300

    :bricks: ci: Project file structure

--- starbooks/src/app/page.tsx
+++ starbooks/src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
-          </h2>
+          </h2> 
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
