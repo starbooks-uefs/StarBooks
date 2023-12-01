ebdd02992864d6e87fa8eda27e0edb500a930662
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-12-01 16:48:43 -0300

    atualizando app admin, arquivo models.py

--- backend/admin/models.py
+++ backend/admin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    password = models.CharField(max_length=16)
+    email = models.CharField(unique=True, max_length=60)
+    birthdate = models.DateTimeField()
+    phone_number = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'admin'
+
+
+
