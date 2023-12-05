6bf7b2eb243fbf3d8ee5016a553deccfc32a9944
Ricardo Campos <rickoliver001@hotmail.com>
2023-12-05 17:47:39 -0300

    :sparkles: feat: Cart model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+# Importando o modelo do Book:
+from book.models import Book
+# Importando o modelo do Book:
+from reader.models import Reader
+
 # Create your models here.
+class Cart(models.Model):
+    id_book = models.OneToOneField(Book, models.DO_NOTHING, db_column='id_book', primary_key=True)  # The composite primary key (id_book, id_reader) found, that is not supported. The first column is selected.
+    id_reader = models.ForeignKey(Reader, models.DO_NOTHING, db_column='id_reader')
+
+    class Meta:
+        managed = False
+        db_table = 'cart'
+        unique_together = (('id_book', 'id_reader'),)
