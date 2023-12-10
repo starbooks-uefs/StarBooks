c5767b6d82f368ad9c8d52fe2cb95c1ec904189d
lpaivao <lpaivauefs@gmail.com>
2023-12-09 23:59:50 -0300

    correção de  urls

--- backend/reader/urls.py
+++ backend/reader/urls.py
@@ -5,11 +5,7 @@ from .views import ReaderListCreateView, ReaderRetrieveUpdateDestroyView, reader
 
 # URLS que serão chamas no arquivo urls.py da pasta principal do projeto
 urlpatterns = [
-    
-    path('api/readers/', ReaderListCreateView.as_view(), name='reader-list-create'),
-    path('api/readers/<uuid:pk>/', ReaderRetrieveUpdateDestroyView.as_view(), name='reader-retrieve-update-destroy'),
-    path('api/accounts/login/', reader_login, name='reader-login'),
-    
-    #path('api/teste1/', views.reader_list, name='teste1'),
-    #path('api/teste2/<uuid:pk>/', views.reader_list, name='teste2'),
+    path('readers/', ReaderListCreateView.as_view(), name='reader-list-create'),
+    path('readers/<uuid:pk>/', ReaderRetrieveUpdateDestroyView.as_view(), name='reader-retrieve-update-destroy'),
+    path('accounts/reader/login/', reader_login, name='reader-login'),
 ]
