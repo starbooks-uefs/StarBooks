32bc7c14dc77313689ff5f52517d17aa93b24618
lpaivao <lpaivauefs@gmail.com>
2023-12-16 00:26:23 -0300

    att id book

--- backend/book/models.py
+++ backend/book/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from producer.models import Producer
 
 class Book(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     gender = models.CharField(max_length=20)
     publisher = models.CharField(max_length=20, blank=True, null=True)
@@ -12,7 +12,7 @@ class Book(models.Model):
     pdf_url = models.CharField(max_length=255)
     price = models.FloatField()
     pages_number = models.IntegerField()
-    date = models.DateTimeField()
+    date = models.DateField()
     cover_url = models.CharField(max_length=255)
     rating = models.IntegerField(blank=True, null=True)
     language = models.CharField(max_length=25, blank=True, null=True)
@@ -21,5 +21,5 @@ class Book(models.Model):
     submission_reason = models.TextField()
 
     class Meta:
-      #  managed = False
+        #managed = False
         db_table = 'book'
