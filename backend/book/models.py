bc71d19753bf7f808a3d3b4cf7266c4593bf7d55
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2023-12-17 20:09:24 -0300

    Refatorando Book Model

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from producer.models import Producer
 
+from enumchoicefield import ChoiceEnum, EnumChoiceField
+
+class SubmissionStatus(ChoiceEnum):
+    approved = "Aprovado"
+    disapproved = "Reprovado"
+    pending = "Pendente"
+
+
 class Book(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=20)
+    author = models.CharField(max_length=50)
+    gender = models.TextField()
     publisher = models.CharField(max_length=20, blank=True, null=True)
     id_producer = models.ForeignKey(Producer, models.DO_NOTHING, db_column='id_producer')
     edition = models.IntegerField()
@@ -16,8 +25,8 @@ class Book(models.Model):
     cover_url = models.CharField(max_length=255)
     rating = models.IntegerField(blank=True, null=True)
     language = models.CharField(max_length=25, blank=True, null=True)
-    submission_status = models.BooleanField()
-    submission_date = models.DateTimeField()
+    submission_status = EnumChoiceField(SubmissionStatus, default=SubmissionStatus.pending)
+    submission_date = models.DateTimeField(auto_now_add=True, blank=True)
     submission_reason = models.TextField()
 
     class Meta:
