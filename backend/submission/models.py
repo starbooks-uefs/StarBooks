d44ec2e19d83cc7c62bd2ff0ad4158a57157aec5
DisneyCarvalho <114960349+DisneyCarvalho@users.noreply.github.com>
2023-12-17 21:45:49 -0300

    Model submission

--- backend/submission/models.py
+++ backend/submission/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from adm.models import Admin
+from producer.models import Producer
+from book.models import Book
 
-# Create your models here.
+
+class Submission(models.Model):
+
+    data = models.DateField(default="")
+    motivo = models.CharField(max_length=25,default='')
+    status = models.CharField(max_length=25,default='')
+    id_book = models.ForeignKey(Book, on_delete=models.CASCADE,default=None)
+    id_Adm = models.ForeignKey(Admin, on_delete=models.DO_NOTHING,default=None)
+    id_producer = models.ForeignKey(Producer, on_delete=models.DO_NOTHING,default=None)
+    
+    class Meta:
+        managed = False
+        db_table = 'submission'
