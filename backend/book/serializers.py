e764d3021701feb5fe0208b42f32b3441b70e652
lpaivao <lpaivauefs@gmail.com>
2023-12-19 23:54:33 -0300

    serializador com todos os campos

--- backend/book/serializers.py
+++ backend/book/serializers.py
@@ -4,8 +4,7 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['name', 'gender','author', 'publisher', 'id_producer', 'edition', 'synopsis', 'pdf_url', 'price', 'pages_number', 'date',
-                  'cover_url', 'rating', 'language', 'submission_status', 'submission_date']
+        fields = '__all__'
 
 # Atualizar Ebook
 class UpdateBookPriceSerializer(serializers.ModelSerializer):
