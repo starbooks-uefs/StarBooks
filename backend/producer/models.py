97eee64d31a2f0ddbc4edb4573996f89437b8f2b
lpaivao <lpaivauefs@gmail.com>
2023-12-27 14:54:27 -0300

    cpf/cnpj agora são do tipo Char

--- backend/producer/models.py
+++ backend/producer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Producer(get_user_model()):
-    cnpj = models.IntegerField(unique=True)
+    cnpj = models.CharField(max_length=14, unique=True)
     bank_name = models.CharField(max_length=25)
     bank_agency = models.IntegerField()
     number_account = models.IntegerField()
--- backend/reader/models.py
+++ backend/reader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Reader(get_user_model()):
-    cpf = models.IntegerField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
     gender = models.CharField(max_length=15, blank=True, null=True)
     cardholder = models.CharField(max_length=50)
     cvv = models.IntegerField()
