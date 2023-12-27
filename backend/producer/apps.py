4507f16430e15b593e70aa0d1fb16e26a5b76ffb
lpaivao <lpaivauefs@gmail.com>
2023-12-27 15:03:21 -0300

    roda os signals

--- backend/producer/apps.py
+++ backend/producer/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ProducerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'producer'
+    
+    def ready(self):
+        import producer.signals
--- backend/reader/apps.py
+++ backend/reader/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ReaderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reader'
+    
+    def ready(self):
+        import reader.signals
