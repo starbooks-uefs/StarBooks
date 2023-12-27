24822d9aa4b51beb1d3ee34949d3446c749e1257
lpaivao <lpaivauefs@gmail.com>
2023-12-27 14:53:21 -0300

    login e logout dos admins

--- backend/adm/views.py
+++ backend/adm/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Admin
 from .serializers import AdminSerializer, LoginAdminSerializer
 
@@ -23,22 +24,37 @@ class AdminRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     # Define as permissões (apenas usuários autenticados podem acessar)
     #permission_classes = [IsAuthenticated]
 
-@api_view(['POST'])
-@permission_classes([])
-def admin_login(request):
-    if request.method == 'POST':
-        serializer = LoginAdminSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data
-            
-            # Criar token
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            
-            # Utilize um serializer para converter o objeto Admin para um formato serializável
-            credentials = {"email": user.email, "password": user.password}
-            
-            return Response({'message': 'Login successful', 'credentials': credentials, 'access_token': access_token}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class AdminLoginView(APIView):
+    permission_classes = [AllowAny]
 
-    return Response({'message': 'Invalid request method'}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        try:
+            user = Admin.objects.get(email=email, password=password)
+        except Admin.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        return Response({'access_token': access_token, 'refresh_token': refresh_token},status=status.HTTP_200_OK)
+    
+class AdminLogoutView(APIView):
+    #permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get("refresh_token")
+
+        if not refresh_token:
+            return Response({"error": "Refresh token is required."})
+
+        try:
+            RefreshToken(refresh_token).blacklist()
+            return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
