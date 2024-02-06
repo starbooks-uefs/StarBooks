640746f356d59f420bc8666911c667e5dbaf091a
lpaivao <lpaivauefs@gmail.com>
2024-02-06 19:41:54 -0300

    numero da conta agora é char

--- backend/producer/models.py
+++ backend/producer/models.py
@@ -5,7 +5,7 @@ class Producer(get_user_model()):
     cnpj = models.CharField(max_length=14, unique=True)
     bank_name = models.CharField(max_length=25)
     bank_agency = models.IntegerField()
-    number_account = models.IntegerField()
+    number_account = models.CharField(max_length=20)
     account_type = models.CharField(max_length=10)
 
     class Meta:
