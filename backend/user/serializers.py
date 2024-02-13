50f960ceaf99090ff67e69fcd3bd990c5d388595
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2024-02-13 15:21:41 -0300

    Create api to change user password

---
+++ backend/user/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
