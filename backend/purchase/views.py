6c50d2bc73466977d3c87b68b3a2b3a08a8b4b58
Gabriel Vitor <gabrielvitortec@gmail.com>
2024-02-14 03:31:13 -0300

    adicionando métricas para o bestseller (livro mais vendido)

--- backend/purchase/views.py
+++ backend/purchase/views.py
@@ -31,3 +31,31 @@ def get_purchase_by_id(request, id_book):
 
   # Retorna um dicionário em formato JSON
   return JsonResponse({'purchase_count': purchase_count, 'total_revenue': total_revenue, 'price': price})
+
+
+def get_bestseller(request):
+  """
+  Função para obter o livro mais vendido e o valor total arrecadado, retornando um JSON.
+
+  Args:
+    request: requisição HTTP
+
+  Returns:
+    Dicionário em formato JSON com o ID do livro, nome, quantidade de compras e valor total arrecadado
+  """
+
+  # Busca o livro mais vendido
+  try:
+    bestseller = Book.objects.annotate(purchase_count=Count('purchases')).order_by('-purchase_count')[0]
+    purchase_count = bestseller.purchase_count
+  except Book.DoesNotExist:
+    purchase_count = 0
+
+  # Busca o preço do ebook
+  price = bestseller.price
+
+  # Calcula o valor total arrecadado
+  total_revenue = purchase_count * price
+
+  # Retorna um dicionário em formato JSON
+  return JsonResponse({'id_book': bestseller.id, 'name': bestseller.name, 'purchase_count': purchase_count, 'total_revenue': total_revenue})
