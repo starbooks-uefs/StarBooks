37a8b4ef7d6ea84e3b061f33856e60b0604ba415
lpaivao <lpaivauefs@gmail.com>
2024-02-14 23:21:15 -0300

    conserto do model de submission

--- backend/submission/models.py
+++ backend/submission/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from adm.models import Admin
-from producer.models import Producer
 from book.models import Book
 
 
 class Submission(models.Model):
+    id_admin = models.OneToOneField(Admin, models.DO_NOTHING, db_column='id_admin', primary_key=True)  # The composite primary key (id_admin, id_book) found, that is not supported. The first column is selected.
+    id_book = models.ForeignKey(Book, models.DO_NOTHING, db_column='id_book')
 
-    data = models.DateField(default="")
-    motivo = models.CharField(max_length=25,default='')
-    status = models.CharField(max_length=25,default='')
-    id_book = models.ForeignKey(Book, on_delete=models.CASCADE,default=None)
-    id_Adm = models.ForeignKey(Admin, on_delete=models.DO_NOTHING,default=None)
-    id_producer = models.ForeignKey(Producer, on_delete=models.DO_NOTHING,default=None)
-    
     class Meta:
         managed = False
         db_table = 'submission'
+        unique_together = (('id_admin', 'id_book'),)
