dad0dd9ef9ddd7d22c8e0519a075f319059ca664
duasck <@Dudu0017>
2024-02-15 21:55:38 -0300

    ...

--- backend/reader/models.py
+++ backend/reader/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth import get_user_model
 class Reader(get_user_model()):
     cpf = models.CharField(max_length=11, unique=True)
     gender = models.CharField(max_length=15, blank=True, null=True)
-    cardholder = models.CharField(max_length=50)
-    cvv = models.IntegerField()
-    card_number = models.CharField(max_length=20)
-    card_date = models.DateField()
+    cardholder = models.CharField(max_length=50, blank=True, null=True)
+    cvv = models.IntegerField(blank=True, null=True)
+    card_number = models.CharField(max_length=20, blank=True, null=True)
+    card_date = models.DateField(blank=True, null=True)
     
     class Meta:
         #managed = False
