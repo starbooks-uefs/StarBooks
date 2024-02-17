50bcc89c3405e78f9b5c8f287204c79827bd7b1e
lpaivao <lpaivauefs@gmail.com>
2024-02-16 23:53:43 -0300

    mudanças nos campos do producer

--- backend/producer/models.py
+++ backend/producer/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 
 class Producer(get_user_model()):
     cnpj = models.CharField(max_length=14, unique=True)
-    bank_name = models.CharField(max_length=25)
-    bank_agency = models.IntegerField()
-    number_account = models.CharField(max_length=20)
-    account_type = models.CharField(max_length=10)
+    bank_name = models.CharField(max_length=25, blank=True, null=True)
+    bank_agency = models.IntegerField(blank=True, null=True)
+    number_account = models.CharField(max_length=20, blank=True, null=True)
+    account_type = models.CharField(max_length=10, blank=True, null=True)
 
     class Meta:
         #managed = False
